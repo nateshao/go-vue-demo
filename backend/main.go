7a6ce1e14657075edf171820e65c449ac779db82
nateshao <shaotongjie@gmail.com>
2021-05-16 21:55:38 +0800

    init

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
 
 func main()  {
 	fmt.Println("hello world!")
+	r := gin.Default()
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(200,gin.H{
+			"message":"你好",
+		})
+
+	})
+	panic(r.Run())
 }
