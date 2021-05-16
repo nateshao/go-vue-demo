decbcb48b7e03910ae225d78cfaad85b2ae15242
nateshao <shaotongjie@gmail.com>
2021-05-16 23:00:58 +0800

    init

--- backend/main.go
+++ backend/main.go
@@ -1,19 +1,92 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-
 func main()  {
-	fmt.Println("hello world!")
-	r := gin.Default()
-	r.GET("/hello", func(c *gin.Context) {
-		c.JSON(200,gin.H{
-			"message":"你好",
-		})
+	//fmt.Println("hello world!")
+	//r := gin.Default()
+	//r.GET("/hello", func(c *gin.Context) {
+	//	c.JSON(200,gin.H{
+	//		"message":"你好",
+	//	})
+	//
+	//})
+	//panic(r.Run())
 
+	r := gin.Default()
+	r.GET("/api/auth/register", func(c *gin.Context) {
+		// 获取参数
+		name := c.PostForm("name")
+		telephone := c.PostForm("telephone")
+		password := c.PostForm("password")
+		if len(telephone) != 11 {
+			c.JSON(http.StatusUnprocessableEntity,gin.H{
+				"code":422,
+				"msg":"手机号必须为11位",
+			})
+		}
+		if len(password) != 1 {
+			
+		}
+		if len(name) != 1 {
+			
+		}
+		// 判断手机号不存在
+
+		// 
 	})
-	panic(r.Run())
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
